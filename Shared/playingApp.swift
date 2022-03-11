8c4b52b0ecae759a0b190411deb4454bbadf558f
Baldwin <17806289931@163.com>
2022-03-11 15:46:43 +0800

    Initial Commit

---
+++ Shared/playingApp.swift
@@ -0,0 +1,17 @@
+//
+//  playingApp.swift
+//  Shared
+//
+//  Created by Baldwin on 2022/3/11.
+//
+
+import SwiftUI
+
+@main
+struct playingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
